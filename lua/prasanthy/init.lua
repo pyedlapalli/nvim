37012387ec415c74da0bf15071dbe8fe0778845d
Prasanth Yedlapalli <pyedlapalli@gmail.com>
2024-09-17 23:58:59 -0400

    added lazyvim, edited some remaps, and added many new plugins:
    bufferline
    neogit
    plenary
    telescope
    nvim-tree
    vimbegood
    which-key

--- lua/prasanthy/init.lua
+++ lua/prasanthy/init.lua
@@ -1,2 +1,3 @@
 require("prasanthy.remap")
 require("prasanthy.options")
+require("prasanthy.lazy")
--- lua/prasanthy/remap.lua
+++ lua/prasanthy/remap.lua
@@ -1,6 +1,30 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Project View" })
+-- Open Lazy plugin manager
+vim.keymap.set("n", "<leader>;", function()
+    vim.cmd("Lazy")
+end)
+
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Project View" })
+vim.keymap.set("n", "<leader>bb",function()
+    vim.cmd("NvimTreeToggle")
+end, { desc = "view nvim tree" })
+
+vim.keymap.set("n", "<leader>pv", "<C-w>w", { desc = "toggle tree buffer" })
+
+-- View project (either through standard :Ex or nvim tree)
+-- make current file executable (for source)
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", 
+    {
+        desc = "make current file executable",
+        silent = true 
+    }
+)
+
+-- source current file
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end, { desc = "source file" })
 
 -- move blocks of code up/down in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -20,14 +44,14 @@ vim.keymap.set("n", "<leader>n", function()
 end)
 
 -- preserve copied text when pasting on highlighted word
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "special reg paste" })
 
 -- paste into system clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "paste to clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "paste to clipboard" })
 
 -- delete to void register to avoid overwriting clipboard
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]], { desc = "special delete" })
 
 -- quick fix centering behavior
 vim.keymap.set("n", "<Cd-k>", "<cmd>cnext<CR>zz")
@@ -36,12 +60,5 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 --replace word that cursor is on
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "replace current word" })
 
--- make current file executable (for source)
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
--- source current file
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
