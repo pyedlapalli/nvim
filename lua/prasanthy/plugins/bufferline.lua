e2aa606e6aad2a3416d931d78f75bdc8c8be7302
Prasanth Yedlapalli <pyedlapalli@gmail.com>
2024-09-19 00:33:00 -0400

    add bufferline for file tabs, git signs for git line highlighting, and lualine for status bar at bottom

--- lua/prasanthy/plugins/bufferline.lua
+++ lua/prasanthy/plugins/bufferline.lua
@@ -1,12 +1,30 @@
-return {
+return{
 
-  "willothy/nvim-cokeline",
-  
-  dependencies = {
-    "nvim-lua/plenary.nvim",        -- Required for v0.4.0+
-    "nvim-tree/nvim-web-devicons", -- If you want devicons
-    "stevearc/resession.nvim"       -- Optional, for persistent history
-  },
-  
-  config = true
+    'romgrk/barbar.nvim',
+
+    dependencies = {
+        'lewis6991/gitsigns.nvim',
+        'nvim-tree/nvim-web-devicons',
+    },
+
+    init = function() vim.g.barbar_auto_setup = false end,
+
+    opts = {},
+
+    version = '^1.0.0', 
+
+    config = function()
+        require('barbar').setup({
+            sidebar_filetypes = {
+                -- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
+                NvimTree = true,
+                -- Or, specify the text used for the offset:
+                undotree = {
+                    text = 'undotree',
+                    align = 'center', -- *optionally* specify an alignment (either 'left', 'center', or 'right')
+                },
+            }
+        })
+    end
 }
+
