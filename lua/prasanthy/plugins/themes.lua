32e18898669a38b2c98d72cc0d22d84f6d112c28
pyedlapalli <pyedlapalli@gmail.com>
2024-09-18 20:14:34 -0400

    add lots of themes

--- lua/prasanthy/plugins/themes.lua
+++ lua/prasanthy/plugins/themes.lua
@@ -1,31 +1,59 @@
 return {
-
-    -- THEME SWITCHER --
-
-    {
-        "zaldih/themery.nvim",
-
-        config = function()
-            require('themery').setup({
-                themes = { "cyberdream", "miasma" }, -- Your list of installed colorschemes.
-                livePreview = true, -- Apply theme while picking. Default to true.
-            })
-        end
-    },
-
-
-
-    -- THEMES --
-    
     {
         "scottmckendry/cyberdream.nvim",
         lazy = false,
         priority = 1000,
+        name = "cyberdream"
     },
-
     {
         "xero/miasma.nvim",
         lazy = false,
         priority = 1000,
+        name = "miasma"
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine"
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    { 
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000 
+    },
+    { 
+        "EdenEast/nightfox.nvim",
+        name = "nightfox",
+        priority = 1000 
+    },
+    { 
+        "neanias/everforest-nvim",
+        name = "everforest",
+        priority = 1000 
+    },
+    { 
+        "navarasu/onedark.nvim",
+        name = "onedark",
+        priority = 1000 
+    },
+    { 
+        "Mofiqul/dracula.nvim",
+        name = "dracula",
+        priority = 1000 
+    },
+    { 
+        "ray-x/aurora",
+        name = "aurora",
+        priority = 1000 
+    },
+    { 
+        "gbprod/nord.nvim",
+        name = "nord",
+        priority = 1000 
     },
 }
+
