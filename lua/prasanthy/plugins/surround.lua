aeaf2be5fefffd99efd53bcd94f03792d567a18c
Prasanth Yedlapalli <pyedlapalli@gmail.com>
2024-09-22 02:22:06 -0400

    switched to nvim-surround for surround keymaps

--- lua/prasanthy/plugins/surround.lua
+++ lua/prasanthy/plugins/surround.lua
@@ -1,10 +1,12 @@
 return {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({})
+    end
 
-    'echasnovski/mini.surround',
-
-    version = false,
-
-    config = function ()
-        require('mini.surround').setup({})
-    end,
+    -- Notes for keymaps --
+    -- add, delete, change
+    -- ys{motion}{char}, ds{char}, and cs{target}{replacement}
 }
